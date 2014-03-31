c9812cfab7bf1b6a0fffa6140139e26bb3e2a75f
Kuba Marek <blue.cube@seznam.cz>
2014-03-31 13:51:18 +0200

    Tree building takes velocities into account as well as positions.

--- ParticleTree/Building.hs
+++ ParticleTree/Building.hs
@@ -4,7 +4,15 @@ import ParticleTree
 import Particles
 import Util.Geometry
 
-type CandidateBox = ([Particle], Int, BBoxF)
+-- | Scale for velocity in the 4D build vectors.
+-- | Higher value means more grouping by velocity, lower value means more
+-- | grouping by position
+velocityScaling :: Float
+velocityScaling = 1
+
+type BuildVector = V4F
+type BuildBBox = BoundingBox V4F
+type CandidateBox = ([Particle], Int, BuildBBox)
 
 rebuildParticleTree :: ParticleTree -> ParticleTree
 rebuildParticleTree = buildParticleTree . particleTreeToList
@@ -16,41 +24,47 @@ buildParticleTree ps = let
     (ps1, ps2, bbox, mg, maxD0) = split ps
     in InnerNode bbox mg maxD0 (buildParticleTree ps1) (buildParticleTree ps2)
 
+particleBuildVector :: Particle -> BuildVector
+particleBuildVector p = let
+    (Vector2D x y) = particlePosition p
+    (Vector2D z w) = (particleVelocity p) |*| velocityScaling
+    in Vector4D x y z w
+
 -- | Greedy splitting algorithm for lists of Particles
 -- | Attempts to put each element into one of the candidate boxes,
 -- | comparing weight functions to find out which one to use.
 split :: [Particle] -> ([Particle], [Particle], BBoxF, Float, Float)
 split particles = let
-    pos1 = particlePosition (head particles)
+    pos1 = particleBuildVector (head particles)
     pos2 = findFurthest pos1 particles pos1 0
     pos3 = findFurthest pos2 particles pos2 0
     in
-    split' particles ([], 0, (BoundingBox pos2 pos2)) ([], 0, (BoundingBox pos3 pos3)) 0 0
-split' :: [Particle] -> CandidateBox -> CandidateBox -> Float -> Float -> ([Particle], [Particle], BBoxF, Float, Float)
-split' [] l r mg maxD0 = let
+    split' particles ([], 0, (BoundingBox pos2 pos2)) ([], 0, (BoundingBox pos3 pos3)) emptyBoundingBox 0 0
+split' :: [Particle] -> CandidateBox -> CandidateBox -> BBoxF -> Float -> Float -> ([Particle], [Particle], BBoxF, Float, Float)
+split' [] l r bbox mg maxD0 = let
     (lList, _, lBBox) = l
     (rList, _, rBBox) = r
-    bbox = updateBoundingBox lBBox rBBox
     in (lList, rList, bbox , mg, maxD0)
-split' (p:ps) l r mg maxD0 = let
+split' (p:ps) l r bbox mg maxD0 = let
     (lList, lCount, lBBox) = l
     (rList, rCount, rBBox) = r
 
-    pos = particlePosition p
+    buildPos = particleBuildVector p
 
-    lUpdated = ((p:lList), lCount + 1, (updateBoundingBoxVector lBBox pos))
-    rUpdated = ((p:rList), rCount + 1, (updateBoundingBoxVector rBBox pos))
+    lUpdated = ((p:lList), lCount + 1, (updateBoundingBoxVector lBBox buildPos))
+    rUpdated = ((p:rList), rCount + 1, (updateBoundingBoxVector rBBox buildPos))
 
     lCost = splitCost lUpdated r
     rCost = splitCost l rUpdated
 
+    newBBox = updateBoundingBoxVector bbox (particlePosition p)
     newMg = mg + (particleMg p)
     newMaxD0 = max maxD0 (particleD0 p)
     in
     if lCost < rCost then
-        split' ps lUpdated r newMg newMaxD0
+        split' ps lUpdated r newBBox newMg newMaxD0
     else
-        split' ps l rUpdated newMg newMaxD0
+        split' ps l rUpdated newBBox newMg newMaxD0
 
 splitCost :: CandidateBox -> CandidateBox -> Float
 splitCost l r = let
@@ -58,10 +72,10 @@ splitCost l r = let
     (_, rCount, rBBox) = r
     in (fromIntegral (abs (lCount - rCount))) + (boundingBoxDiameter lBBox) + (boundingBoxDiameter rBBox)
 
-findFurthest :: V2F -> [Particle] -> V2F -> Float -> V2F
+findFurthest :: BuildVector -> [Particle] -> BuildVector -> Float -> BuildVector
 findFurthest _ [] pos _ = pos
 findFurthest pos1 (p:ps) best bestDistance = let
-    pos2 = particlePosition p
+    pos2 = particleBuildVector p
     distance = absSquared (pos1 |-| pos2)
     in
     if distance > bestDistance then
